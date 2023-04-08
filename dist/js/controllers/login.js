2732718dba0c5874142f58f4a511b26288099ed8
vitalkramer <vitalkramer@hotmail.com>
2023-04-07 23:51:50 -0300

    Subir

---
+++ dist/js/controllers/login.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Login = void 0;
+class Login {
+    constructor() {
+        this.imputLogin = document.querySelector('#login');
+        this.imputSenha = document.querySelector('senha');
+    }
+}
+exports.Login = Login;
---
+++ src/controllers/login.ts
@@ -0,0 +1,12 @@
+
+export class Login {
+
+    private imputLogin;
+    private imputSenha;
+
+    constructor(){
+        this.imputLogin = document.querySelector('#login');
+        this.imputSenha = document.querySelector('senha');
+    }
+
+}
